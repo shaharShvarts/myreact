567f7a03e7f4eec0bf668b8a4a09cecc5fc7394b
shahar shvarts <shahar.shvarts@gmail.com>
2020-05-13 07:42:49 +0300

    Updates

---
+++ precache-manifest.6bc384dc302c7666809dccab0ad56cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd3205ccd910a34d199f8b2022a6514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cdb79c76ca962b824d7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61f233c12b27ee332213",
+    "url": "/static/js/2.736ef6c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.736ef6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cdb79c76ca962b824d7",
+    "url": "/static/js/main.c0e93de5.chunk.js"
+  },
+  {
+    "revision": "8e5a951bc4fa01d3763a",
+    "url": "/static/js/runtime-main.569b10b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
